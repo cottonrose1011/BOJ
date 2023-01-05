d6866f8aa4e64193f11380d909da8df9d15f10f3
cottonrose1011 <pjeka7er43@gmail.com>
2023-01-05 21:09:36 +0900

    약수의 정의를 이용한 소수 판별

---
+++ Python/BOJ1929.py
@@ -0,0 +1,17 @@
+import math
+
+
+def isPrime(num):
+    if num == 1:
+        return False
+    for i in range(2, int(math.sqrt(num))+1):
+        if num % i == 0:
+            return False
+    return True
+
+
+m, n = map(int, input().split())
+
+for i in range(m, n+1):
+    if isPrime(i):
+        print(i)
